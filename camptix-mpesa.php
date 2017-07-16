24a1eaf21a1b90d82b3e65fc30c94d5c318655d6
Emmanuel Chekumbe <emcie4@gmail.com>
2017-07-16 09:00:05 +0300

    Added KES Shilling

--- camptix-mpesa.php
+++ camptix-mpesa.php
@@ -1,1 +1,33 @@
 
+<?php
+/**
+* Plugin Name: CampTix Mpesa Payment gateway
+* Plugin URI: https://nabaleka.com
+* Description: Mpesa Payment Gateway for CampTix
+* Author: Emmanuel Ammanulah
+* Author URI: https://nabaleka.com
+* Version: 1.0.0
+* Licence: GPLv2 or later
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Add KES currency
+add_filter( 'camptix_currencies', 'camptix_add_kes_currency' );
+function camptix_add_kes_currency( $currencies ) {
+  $currencies['KES'] = array(
+    'label' => __( 'Kenyan Shilling', 'camptix' ),
+    'format' => 'Ksh %s',
+  );
+  return $currencies;
+}
+
+// Load the Mpesa Payment Method
+add_action( 'camptix_load_addons', 'camptix_mpesa_load_payment_method' );
+function camptix_mpesa_load_payment_method() {
+  if ( ! class_exists( 'Mpesa_Camptix' ) )
+    require_once plugin_dir_path( __FILE__ ) . 'classes/payment-mpesa.php';
+  camptix_register_addon( 'Mpesa_Camptix' );
+}
+
+ ?>
